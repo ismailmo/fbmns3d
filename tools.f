f299174d354c7e62702c5f6cea7958b52ca40066
ismail.mourad@gmail.com <ismail.mourad@gmail.com@37ce39c2-a353-0410-bfb6-adb894a945c8>
2009-10-18 16:29:19 +0000

    test  variable

--- tools.f
+++ tools.f
@@ -15,6 +15,8 @@ c  You should have received a copy of the GNU General Public License
 c  along with Fbmns3d. If not, see <http://www.gnu.org/licenses/>.
 
 c Author : Mourad Ismail   (ismail@ujf-grenoble.fr)
+
+
 C	$Id$	
 
 
